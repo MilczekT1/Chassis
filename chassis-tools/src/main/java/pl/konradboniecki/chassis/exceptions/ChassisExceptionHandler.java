36fbbeb36be237ba68ee307aae834ca2a23577a7
Konrad.Boniecki <konrad.boniecki@finastra.com>
2019-06-12 14:49:21 +0200

    repackaged ErrorDescription

--- chassis-tools/src/main/java/pl/konradboniecki/chassis/exceptions/ChassisExceptionHandler.java
+++ chassis-tools/src/main/java/pl/konradboniecki/chassis/exceptions/ChassisExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import pl.konradboniecki.chassis.ErrorDescription;
 
 @Slf4j
 @ControllerAdvice

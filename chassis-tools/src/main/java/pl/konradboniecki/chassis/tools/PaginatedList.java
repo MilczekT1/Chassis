97c33be9042af46ade7402f25f6e27737bb07b4c
Konrad Boniecki <konrad_boniecki@hotmail.com>
2019-10-21 22:31:32 +0200

    Added paginatedList  to chassis tools

---
+++ chassis-tools/src/main/java/pl/konradboniecki/chassis/tools/PaginatedList.java
@@ -0,0 +1,21 @@
+package pl.konradboniecki.chassis.tools;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+public class PaginatedList<T> {
+
+    private List<T> items;
+    @JsonProperty("_meta")
+    private PaginationMetadata paginationMetadata;
+
+    public PaginatedList(@NonNull Page<T> page) {
+        this.items = page.getContent();
+        this.paginationMetadata = new PaginationMetadata(page);
+    }
+}

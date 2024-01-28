753bdb96cb0912c7a0be67f716438e6d329e6fa7
konradboniecki <konrad_boniecki@hotmail.com>
2024-01-28 18:22:10 +0100

    Added @ControllerTest annotation with test listener

---
+++ chassis-test-tools/src/main/java/pl/konradboniecki/chassis/testtools/TestContextUtils.java
@@ -0,0 +1,40 @@
+package pl.konradboniecki.chassis.testtools;
+
+import lombok.NoArgsConstructor;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestContextAnnotationUtils;
+
+import java.lang.annotation.Annotation;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+
+
+@NoArgsConstructor
+public class TestContextUtils {
+
+    public static <T> T getBean(TestContext testContext, Class<T> beanClass) {
+        return testContext.getApplicationContext().getBean(beanClass);
+    }
+
+    public static <T> Optional<T> getOptionalBean(TestContext testContext, Class<T> beanClass) {
+        var beanProvider = testContext.getApplicationContext().getBeanProvider(beanClass);
+        return Optional.ofNullable(beanProvider.getIfAvailable());
+    }
+
+    public static <A extends Annotation> Optional<A> getTestClassAnnotation(
+            TestContext testContext, Class<A> annotation
+    ) {
+        var descriptor = TestContextAnnotationUtils.findAnnotationDescriptor(testContext.getTestClass(), annotation);
+        return ofNullable(descriptor)
+                .map(TestContextAnnotationUtils.AnnotationDescriptor::getAnnotation);
+    }
+
+    public static <A extends Annotation> Optional<A> getMethodAnnotation(
+            TestContext testContext,
+            Class<A> annotationClass
+    ) {
+        return ofNullable(AnnotatedElementUtils.findMergedAnnotation(testContext.getTestMethod(), annotationClass));
+    }
+}

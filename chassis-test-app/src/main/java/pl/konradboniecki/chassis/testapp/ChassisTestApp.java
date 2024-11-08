7e9fc55470d76c498ad2ac97197d1449e236eee2
konradboniecki <konrad_boniecki@hotmail.com>
2024-11-08 19:10:39 +0100

    Added chassis dependencies module and test-app

---
+++ chassis-test-app/src/main/java/pl/konradboniecki/chassis/testapp/ChassisTestApp.java
@@ -0,0 +1,12 @@
+package pl.konradboniecki.chassis.testapp;
+
+import org.springframework.boot.SpringApplication;
+import pl.konradboniecki.chassis.ChassisApplication;
+
+@ChassisApplication
+public class ChassisTestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChassisTestApp.class, args);
+    }
+}

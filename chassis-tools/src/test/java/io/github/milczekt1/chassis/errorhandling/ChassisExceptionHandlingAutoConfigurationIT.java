640e6614b2e7af1ce77761821dea75a55e124105
konradboniecki <konrad_boniecki@hotmail.com>
2024-12-03 11:41:54 +0100

    small test refactor

--- chassis-tools/src/test/java/io/github/milczekt1/chassis/errorhandling/ChassisExceptionHandlingAutoConfigurationIT.java
+++ chassis-tools/src/test/java/io/github/milczekt1/chassis/errorhandling/ChassisExceptionHandlingAutoConfigurationIT.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.contract.spec.internal.RegexPatterns;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static io.github.milczekt1.chassis.errorhandling.utils.TestController.*;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.matchesPattern;
 import static org.springframework.http.HttpStatus.*;
@@ -27,6 +28,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
     @BeforeEach
     void setUp() {
         RestAssuredMockMvc.mockMvc(mvc);
+        RestAssuredMockMvc.basePath = TestController.BASE_PATH;
     }
 
   @Test
@@ -39,7 +41,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
     void shouldReturnValidProblemDetailsForBadRequestException() {
         RestAssuredMockMvc.given()
                 .when()
-                .get(TestController.BAD_REQUEST_PATH)
+                .get(BAD_REQUEST_PATH)
                 .then()
                 .status(BAD_REQUEST)
                 .contentType(APPLICATION_PROBLEM_JSON_VALUE)
@@ -47,7 +49,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
                 .body("title", equalTo("Bad Request"))
                 .body("status", equalTo(BAD_REQUEST.value()))
                 .body("detail", equalTo(TestController.ERROR_TITLE))
-                .body("instance", equalTo(TestController.BAD_REQUEST_PATH))
+                .body("instance", equalTo(BASE_PATH + BAD_REQUEST_PATH))
                 .body("timestamp", matchesPattern(RegexPatterns.iso8601WithOffset().getPattern()));
     }
 
@@ -55,7 +57,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
     void shouldReturnValidProblemDetailsForResourceNotFoundException() {
         RestAssuredMockMvc.given()
                 .when()
-                .get(TestController.NOT_FOUND_PATH)
+                .get(NOT_FOUND_PATH)
                 .then()
                 .status(NOT_FOUND)
                 .contentType(APPLICATION_PROBLEM_JSON_VALUE)
@@ -63,7 +65,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
                 .body("title", equalTo("Not Found"))
                 .body("status", equalTo(NOT_FOUND.value()))
                 .body("detail", equalTo(TestController.ERROR_TITLE))
-                .body("instance", equalTo(TestController.NOT_FOUND_PATH))
+                .body("instance", equalTo(BASE_PATH + NOT_FOUND_PATH))
                 .body("timestamp", matchesPattern(RegexPatterns.iso8601WithOffset().getPattern()));
     }
 
@@ -71,7 +73,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
     void shouldReturnValidProblemDetailsForInternalServerErrorException() {
         RestAssuredMockMvc.given()
                 .when()
-                .get(TestController.INTERNAL_SERVER_ERROR_PATH)
+                .get(INTERNAL_SERVER_ERROR_PATH)
                 .then()
                 .status(INTERNAL_SERVER_ERROR)
                 .contentType(APPLICATION_PROBLEM_JSON_VALUE)
@@ -79,7 +81,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
                 .body("title", equalTo("Internal Server Error"))
                 .body("status", equalTo(INTERNAL_SERVER_ERROR.value()))
                 .body("detail", equalTo(TestController.ERROR_TITLE))
-                .body("instance", equalTo(TestController.INTERNAL_SERVER_ERROR_PATH))
+                .body("instance", equalTo(BASE_PATH + INTERNAL_SERVER_ERROR_PATH))
                 .body("timestamp", matchesPattern(RegexPatterns.iso8601WithOffset().getPattern()));
     }
 
@@ -87,7 +89,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
     void shouldReturnValidProblemDetailsForResourceConflictException() {
         RestAssuredMockMvc.given()
                 .when()
-                .get(TestController.RESOURCE_CONFLICT_ERROR_PATH)
+                .get(RESOURCE_CONFLICT_ERROR_PATH)
                 .then()
                 .status(CONFLICT)
                 .contentType(APPLICATION_PROBLEM_JSON_VALUE)
@@ -95,7 +97,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
                 .body("title", equalTo("Conflict"))
                 .body("status", equalTo(CONFLICT.value()))
                 .body("detail", equalTo(TestController.ERROR_TITLE))
-                .body("instance", equalTo(TestController.RESOURCE_CONFLICT_ERROR_PATH))
+                .body("instance", equalTo(BASE_PATH + RESOURCE_CONFLICT_ERROR_PATH))
                 .body("timestamp", matchesPattern(RegexPatterns.iso8601WithOffset().getPattern()));
     }
 
@@ -103,7 +105,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
     void shouldReturnValidProblemDetailsForResourceCreationException() {
         RestAssuredMockMvc.given()
                 .when()
-                .get(TestController.RESOURCE_CREATION_ERROR_PATH)
+                .get(RESOURCE_CREATION_ERROR_PATH)
                 .then()
                 .status(BAD_REQUEST)
                 .contentType(APPLICATION_PROBLEM_JSON_VALUE)
@@ -111,7 +113,7 @@ class ChassisExceptionHandlingAutoConfigurationIT {
                 .body("title", equalTo("Bad Request"))
                 .body("status", equalTo(BAD_REQUEST.value()))
                 .body("detail", equalTo(TestController.ERROR_TITLE))
-                .body("instance", equalTo(TestController.RESOURCE_CREATION_ERROR_PATH))
+                .body("instance", equalTo(BASE_PATH + RESOURCE_CREATION_ERROR_PATH))
                 .body("timestamp", matchesPattern(RegexPatterns.iso8601WithOffset().getPattern()));
     }
 
--- chassis-tools/src/test/java/io/github/milczekt1/chassis/errorhandling/utils/TestController.java
+++ chassis-tools/src/test/java/io/github/milczekt1/chassis/errorhandling/utils/TestController.java
@@ -11,34 +11,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/exceptions")
 public class TestController {
 
+    public static final String BASE_PATH = "/exceptions";
     public static final String ERROR_TITLE = "Test message";
-    public static final String BAD_REQUEST_PATH = "/exceptions/bad-request";
-    public static final String RESOURCE_CREATION_ERROR_PATH = "/exceptions/bad-request/creation-error";
-    public static final String NOT_FOUND_PATH = "/exceptions/not-found";
-    public static final String INTERNAL_SERVER_ERROR_PATH = "/exceptions/internal-server-error";
-    public static final String RESOURCE_CONFLICT_ERROR_PATH = "/exceptions/conflict";
+    public static final String BAD_REQUEST_PATH = "/bad-request";
+    public static final String RESOURCE_CREATION_ERROR_PATH = "/bad-request/creation-error";
+    public static final String NOT_FOUND_PATH = "/not-found";
+    public static final String INTERNAL_SERVER_ERROR_PATH = "/internal-server-error";
+    public static final String RESOURCE_CONFLICT_ERROR_PATH = "/conflict";
 
-    @GetMapping("/bad-request")
+    @GetMapping(BAD_REQUEST_PATH)
     public void throwBadRequestException() {
         throw new BadRequestException(ERROR_TITLE);
     }
 
-    @GetMapping("/bad-request/creation-error")
+    @GetMapping(RESOURCE_CREATION_ERROR_PATH)
     public void throwResourceCreationException() {
         throw new ResourceCreationException(ERROR_TITLE);
     }
 
-    @GetMapping("/not-found")
+    @GetMapping(NOT_FOUND_PATH)
     public void throwResourceNotFoundException() {
         throw new ResourceNotFoundException(ERROR_TITLE);
     }
 
-    @GetMapping("/internal-server-error")
+    @GetMapping(INTERNAL_SERVER_ERROR_PATH)
     public void throwInternalServerErrorException() {
         throw new InternalServerErrorException(ERROR_TITLE);
     }
 
-    @GetMapping("/conflict")
+    @GetMapping(RESOURCE_CONFLICT_ERROR_PATH)
     public void throwResourceConflictException() {
         throw new ResourceConflictException(ERROR_TITLE);
     }

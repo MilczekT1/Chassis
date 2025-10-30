0f6801aae4476ec20f8cb73fa032c7163550ab58
konradboniecki <konrad_boniecki@hotmail.com>
2025-10-30 17:21:23 +0100

    Adjusted mongo in test-app tests

--- chassis-test-app/src/test/java/io/github/milczekt1/chassis/testapp/utils/TestAppIntegrationTest.java
+++ chassis-test-app/src/test/java/io/github/milczekt1/chassis/testapp/utils/TestAppIntegrationTest.java
@@ -3,9 +3,11 @@ package io.github.milczekt1.chassis.testapp.utils;
 
 import io.github.milczekt1.chassis.test.logging.LogbackVerifierExtension;
 import io.github.milczekt1.chassis.testapp.ChassisTestApp;
+import io.github.milczekt1.chassis.testapp.utils.containers.MongoContainerInitializer;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -17,6 +19,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @ActiveProfiles({"itest"})
 @ExtendWith(LogbackVerifierExtension.class)
+@ContextConfiguration(initializers = {
+        MongoContainerInitializer.class
+})
 @SpringBootTest(classes = ChassisTestApp.class)
 public @interface TestAppIntegrationTest {
 }

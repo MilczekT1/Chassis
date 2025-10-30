6587c412a88617ab7501b1d3cf3d51315d603f06
konradboniecki <konrad_boniecki@hotmail.com>
2025-10-30 18:55:24 +0100

    extended startup timeout of mongo from 90s to 120s.

--- chassis-tools-test/src/test/java/io/github/milczekt1/chassis/test/utils/containers/TestContainerDefinitions.java
+++ chassis-tools-test/src/test/java/io/github/milczekt1/chassis/test/utils/containers/TestContainerDefinitions.java
@@ -2,11 +2,14 @@ package io.github.milczekt1.chassis.test.utils.containers;
 
 import org.testcontainers.containers.MongoDBContainer;
 
+import java.time.Duration;
+
 public final class TestContainerDefinitions {
 
     public static final String MONGODB_DB_NAME = "testDatabase";
 
     public static final MongoDBContainer MONGODB = new MongoDBContainer("mongo:8.0.15")
             .withEnv("MONGO_INITDB_DATABASE", MONGODB_DB_NAME)
-            .withReuse(System.getenv("CI") == null);
+            .withReuse(System.getenv("CI") == null)
+            .withStartupTimeout(Duration.ofMinutes(2));
 }

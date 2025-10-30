4a94d046c5a942ba1a7f5752e178c902719829c6
konradboniecki <konrad_boniecki@hotmail.com>
2025-10-30 19:49:48 +0100

    cleanup

--- chassis-tools-test/src/test/java/io/github/milczekt1/chassis/test/utils/containers/TestContainerDefinitions.java
+++ chassis-tools-test/src/test/java/io/github/milczekt1/chassis/test/utils/containers/TestContainerDefinitions.java
@@ -2,14 +2,11 @@ package io.github.milczekt1.chassis.test.utils.containers;
 
 import org.testcontainers.containers.MongoDBContainer;
 
-import java.time.Duration;
-
 public final class TestContainerDefinitions {
 
     public static final String MONGODB_DB_NAME = "testDatabase";
 
     public static final MongoDBContainer MONGODB = new MongoDBContainer("mongo:8.0.15")
             .withEnv("MONGO_INITDB_DATABASE", MONGODB_DB_NAME)
-            .withReuse(System.getenv("CI") == null)
-            .withStartupTimeout(Duration.ofMinutes(2));
+            .withReuse(System.getenv("CI") == null);
 }

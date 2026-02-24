87a6583390a38d8f0ad5511ee3c6f082b26fa8c9
konradboniecki <konrad_boniecki@hotmail.com>
2026-02-24 19:00:30 +0100

    update test app

--- chassis-test-app/src/test/java/io/github/milczekt1/chassis/testapp/utils/containers/TestContainerDefinitions.java
+++ chassis-test-app/src/test/java/io/github/milczekt1/chassis/testapp/utils/containers/TestContainerDefinitions.java
@@ -1,12 +1,21 @@
 package io.github.milczekt1.chassis.testapp.utils.containers;
 
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
 
-public final class TestContainerDefinitions {
+@TestConfiguration
+public class TestContainerDefinitions {
 
     public static final String MONGODB_DB_NAME = "testDatabase";
 
-    public static final MongoDBContainer MONGODB = new MongoDBContainer("mongo:8.0.15")
-            .withEnv("MONGO_INITDB_DATABASE", MONGODB_DB_NAME)
-            .withReuse(System.getenv("CI") == null);
+    @Bean
+    @ServiceConnection
+    MongoDBContainer mongoDbContainer() {
+        return new MongoDBContainer(DockerImageName.parse("mongo:8.0.15"))
+                .withEnv("MONGO_INITDB_DATABASE", MONGODB_DB_NAME)
+                .withReuse(System.getenv("CI") == null);
+    }
 }

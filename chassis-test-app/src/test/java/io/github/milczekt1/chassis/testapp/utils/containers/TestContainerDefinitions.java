9d18872af559e8f51b8e918fd93b5ae4f7b3e97c
konradboniecki <konrad_boniecki@hotmail.com>
2026-02-24 19:11:27 +0100

    use new container for mongo

--- chassis-test-app/src/test/java/io/github/milczekt1/chassis/testapp/utils/containers/TestContainerDefinitions.java
+++ chassis-test-app/src/test/java/io/github/milczekt1/chassis/testapp/utils/containers/TestContainerDefinitions.java
@@ -3,7 +3,7 @@ package io.github.milczekt1.chassis.testapp.utils.containers;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.mongodb.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
